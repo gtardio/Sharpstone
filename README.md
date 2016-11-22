9572fc11bb098b449ac79f8c012ab4eb894cc7e1
Luca Bozzetto <luca.bozzetto@icloud.com>
2016-11-22 21:00:39 +0100

    Template di partenza del progetto

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Sharpstone
 Progetto modulo 1 esame di programmazione. Sharpstone, un gioco hearthstone-like realizzato in F#
+
+Componenti gruppo:
+Gianluca Tardio
+Carlotta Medana
+Luca Bozzetto
+
+# Informazioni utili
+Commentare le varie funzioni in maniera chiara in modo che tutti possano capire il preciso funzionamento.
+Ricordatevi di descrivere brevemente le modifiche apportate ad ogni commit che eseguite.
+
+Per una guida base sull'utilizzo di GIT vi rimando a qui:
+http://rogerdudler.github.io/git-guide/index.it.html
+
+o qui:
+https://guides.github.com/
+
+Non scordatevi di fare il push alla fine della giornata altrimenti le modifiche non vengono aggiornate online.
+La repository è pubblica per chiunque su Github ma 99% non se la filerà nessuno e a noi poco importa se qualcuno la vede!
+
+link moodle:
+https://moodle.unive.it/mod/assign/view.php?id=36473
+
---
+++ SharpStone/Main.fs
@@ -0,0 +1,136 @@
+﻿(*
+ * Sharpstone: a tiny card game simulator
+ *
+ * Written as project exam template for Computer Science, Laboratorio di Programmazione
+ * Freely adapted from Heartstone (TM) by Blizzard Entertainment, Inc.
+ *
+ * (C) 2016 Alvise Spanò @ DAIS, Università Ca' Foscari, Venezia
+ *)
+
+module LabProg2016.Sharpstone
+
+#if INTERACTIVE
+#r "System.Runtime.Serialization.dll"
+#endif
+
+open System
+open System.IO
+open System.Runtime.Serialization
+open System.Text
+
+// globals
+//
+
+let rand = new Random (23)    // remove seed argument for making randomness indeterministic
+
+/// Generate a random integer within the interval (a, b) inclusively.
+let rnd_int a b = rand.Next (a, b + 1) 
+
+
+// type definitions
+//
+
+/// Defines the card type.
+[< DataContract; StructuralEquality; NoComparison >]
+type card = {
+    [< field: DataMember(Name = "id") >] id : string
+    [< field: DataMember(Name = "name") >] name : string
+    [< field: DataMember(Name = "cost") >] cost : int
+    [< field: DataMember(Name = "type") >] typee : string
+    [< field: DataMember(Name = "attack") >] attack : int
+    [< field: DataMember(Name = "health") >] mutable health : int
+}
+with
+    override c.ToString () = sprintf "%s [Id:%s  Atk:%d  HP:%d]" c.name c.id c.attack c.health
+
+/// Deck type alias.
+type deck = card list
+
+/// Defined the player type.
+[< StructuralEquality; NoComparison >]
+type player = {
+    name : string
+    mutable life : int
+    mutable deck : deck
+}
+with
+    override p.ToString () = sprintf "%s [Life:%d  Deck:%d]" p.name p.life p.deck.Length
+
+
+// JSON stuff
+//
+
+/// Convert a JSON string into a typed value.
+let unjson<'t> (input : string) : 't =  
+    use ms = new MemoryStream(ASCIIEncoding.Default.GetBytes(input)) 
+    let obj = (new Json.DataContractJsonSerializer(typeof<'t>)).ReadObject(ms) 
+    obj :?> 't
+
+/// Parse a JSON deck given the filename.
+let parse_deck (filename : string) = 
+    use fstr = File.OpenRead filename
+    use rd = new StreamReader (fstr)
+    printfn "Parsing JSON file \"%s\"..." fstr.Name
+    rd.ReadToEnd () |> unjson<card[]> |> Array.toList
+
+
+// printers
+//
+
+/// Prints the turn number header. Call this function at the beginning of each turn.
+let print_turn_begin (turn : int) = printfn "\nTurn %d:" turn
+
+/// Prints the status of the 2 players. Call this function at the end of each turn.
+let print_turn_end (p1 : player, p2 : player) = printfn "\t%O\n\t%O" p1 p2
+
+/// Prints the information of 2 cards fighting. Call this function at each turn when both players have a card.
+let print_turn_2cards (c1 : card, c2 : card) = printfn "%O VS %O" c1 c2
+
+/// Prints the information of 1 card fighting against a player with no cards. Call this function at each turn when only 1 players have a card.
+let print_turn_1card (p : player, c : card) = printfn "* %O VS player %O" c p
+
+/// Prints the information of 2 players when both have no cards. Call this function at each turn no cards have been drawn.
+let print_turn_no_cards (p1 : player, p2 : player) = printfn "* Both %O and %O have no cards" p1 p2
+
+/// Prints the information of a dead cards. Call this function when a card dies.
+let print_card_death (c : card) = printfn "+ %O died (%d overkill)" { c with health = 0 } -c.health
+
+
+
+// combat mechanics
+//
+
+// !!! YOU MUST IMPLEMENT THIS !!!
+let fight (deck1 : deck) (deck2 : deck) : player * player * int =
+    let p1 = { name ="P1"; life = 30; deck = deck1 }    // dummy players
+    let p2 = { name ="P2"; life = 30; deck = deck2 }
+    p1, p2, 0
+
+
+
+// main code
+//
+
+[< EntryPoint >]
+let main argv =
+    let code =
+        try
+            if argv.Length <> 2 then
+                printfn "Usage: Sharpstone <DECK1> <DECK2>"
+                0
+            else
+                let p filename = parse_deck filename    // function for parsing a JSON file defining a deck as a list of cards
+                let d1 = p argv.[0]                     // parse the first argument of the executable (DECK1)
+                let d2 = p argv.[1]                     // parse the second argument of the executable (DECK2)
+                let p1, p2, turn as r = fight d1 d2
+                // print final result
+                printfn "\nResult:\n\t%d Turns\n\t%O\n\t%O\n\tHash: %X" turn p1 p2 (r.GetHashCode ())
+                0
+
+        with e -> printfn "Uncaught exception: %O" e; 1
+
+    #if DEBUG
+    printfn "\n\nPress any key to exit..."
+    Console.ReadKey () |> ignore
+    #endif
+    code
